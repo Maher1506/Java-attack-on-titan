48084421ea1c991c59c3b6b220dd75030d830815
Maher1506 <68017554+Maher1506@users.noreply.github.com>
2024-03-10 00:34:03 +0200

    Finished all methods for the interfaces

--- AOT/src/game/engine/interfaces/Attackee.java
+++ AOT/src/game/engine/interfaces/Attackee.java
@@ -1,5 +1,12 @@
 package game.engine.interfaces;
 
+//for all objects that can be attacked
 public interface Attackee {
-
+	
+	//retrieves health
+	int getCurrentHealth();
+	//changes attackee's health
+	void setCurrentHealth(int health);
+	//retrieves resource value of the attackee
+	int getResourceValue();
 }
--- AOT/src/game/engine/interfaces/Mobil.java
+++ AOT/src/game/engine/interfaces/Mobil.java
@@ -1,5 +1,14 @@
 package game.engine.interfaces;
 
+//for all objects that can move
 public interface Mobil {
-
+	
+	//retrieves the mobil's distance from the target
+	int getDistance();
+	//changes the mobil's distance from the target
+	void setDistance(int distance);
+	//retrieves the mobil's speed
+	int getSpeed();
+	//changes the mobil's speed
+	void setSpeed(int speed);
 }
