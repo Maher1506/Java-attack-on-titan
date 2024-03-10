a124b7688c81714d84fc0822ce5aadd74ed090fc
nazmy666 <nazmy2362005@gmail.com>
2024-03-11 01:20:05 +0200

    finished titan subclasses

--- AOT/src/game/engine/titans/ColossalTitan.java
+++ AOT/src/game/engine/titans/ColossalTitan.java
@@ -1,6 +1,9 @@
 package game.engine.titans;
 
 public class ColossalTitan extends Titan {
+	
+	private final int TITAN_CODE = 4;
+	
 	ColossalTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase,
 			int speed, int resourcesValue, int dangerLevel)
 			{
