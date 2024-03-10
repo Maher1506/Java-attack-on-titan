bc18c6c560522ad45eb43d411b29ab1e40719a8a
mariam asaad <mariam.m.m.a2020@gmail.com>
2024-03-10 23:38:09 +0200

    finished all weapons class

--- AOT/src/game/engine/weapons/WallTrap.java
+++ AOT/src/game/engine/weapons/WallTrap.java
@@ -1,5 +1,10 @@
 package game.engine.weapons;
 
-public class WallTrap {
+public class WallTrap extends Weapon{
+	private final int WEAPON_CODE = 4;
+	
+	public WallTrap(int baseDamage){
+		super(baseDamage);
+	}
 
 }
