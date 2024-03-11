2d10598eb1baa236d474171d24dc3fca3cbdcd55
Maher1506 <68017554+Maher1506@users.noreply.github.com>
2024-03-11 15:42:30 +0200

    added attributes for the dataloader class

--- AOT/src/game/engine/dataloader/DataLoader.java
+++ AOT/src/game/engine/dataloader/DataLoader.java
@@ -2,4 +2,8 @@ package game.engine.dataloader;
 
 public class DataLoader {
 
+	private final String TITANS_FILE_NAME = "titans.csv";
+	private final String WEAPONS_FILE_NAME = "weapons.csv";
+	
+	
 }
