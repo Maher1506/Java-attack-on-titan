5285400abd425448ae5e91fe58383b7a9dbc92db
Maher1506 <68017554+Maher1506@users.noreply.github.com>
2024-03-11 20:59:17 +0200

    created dataloader class

--- AOT/src/game/engine/dataloader/DataLoader.java
+++ AOT/src/game/engine/dataloader/DataLoader.java
@@ -1,9 +1,18 @@
 package game.engine.dataloader;
 
+import game.engine.titans.TitanRegistry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class DataLoader {
 
 	private final String TITANS_FILE_NAME = "titans.csv";
 	private final String WEAPONS_FILE_NAME = "weapons.csv";
 	
-	
+	public static HashMap<Integer, TitanRegistry> readTitanRegistry() throws IOException
+	{
+		BufferedReader br = 
+	}
 }
