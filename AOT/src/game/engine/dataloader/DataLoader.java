0a4e5359d93bc5af1df46536c2bbfe2c8115d485
Maher1506 <68017554+Maher1506@users.noreply.github.com>
2024-03-11 23:06:23 +0200

    Finished mayten om el dataloader class

--- AOT/src/game/engine/dataloader/DataLoader.java
+++ AOT/src/game/engine/dataloader/DataLoader.java
@@ -1,6 +1,7 @@
 package game.engine.dataloader;
 
 import game.engine.titans.TitanRegistry;
+import game.engine.weapons.WeaponRegistry;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,20 +10,30 @@ import java.util.HashMap;
 
 public class DataLoader {
 
-	private final String TITANS_FILE_NAME = "titans.csv";
-	private final String WEAPONS_FILE_NAME = "weapons.csv";
+	private final static String TITANS_FILE_NAME = "titans.csv";
+	private final static String WEAPONS_FILE_NAME = "weapons.csv";
 	
-	public static void main(String[] args) throws IOException
+	/*public static void main(String[] args) throws IOException
 	{
-		System.out.println(readTitanRegistry());
-	}
+		for (Integer name: readTitanRegistry().keySet()) {
+		    String key = name.toString();
+		    String value = readTitanRegistry().get(name).toString();
+		    System.out.println(key + " " + value);
+		}
+		System.out.println();
+		for (Integer name: readWeaponRegistry().keySet()) {
+		    String key = name.toString();
+		    String value = readWeaponRegistry().get(name).toString();
+		    System.out.println(key + " " + value);
+		}
+	}*/
 	
 	public static HashMap<Integer, TitanRegistry> readTitanRegistry() throws IOException
 	{
-		HashMap<Integer, TitanRegistry> registry = new HashMap();
+		HashMap<Integer, TitanRegistry> titanHashMap = new HashMap();
 		try
 		{
-			FileReader fr = new FileReader("TITANS_FILE_NAME");
+			FileReader fr = new FileReader(TITANS_FILE_NAME);
 			BufferedReader br = new BufferedReader(fr);
 			
 			String line = "";
@@ -33,11 +44,43 @@ public class DataLoader {
 						Integer.parseInt(temp[1]), Integer.parseInt(temp[2]),
 						Integer.parseInt(temp[3]), Integer.parseInt(temp[4]),
 						Integer.parseInt(temp[5]), Integer.parseInt(temp[6]));
-				registry.put(titanRegistry.getCode(), titanRegistry);
+				titanHashMap.put(titanRegistry.getCode(), titanRegistry);
 			}
 		}
 		catch (IOException e){ e.printStackTrace(); }
 		
-		return registry;
+		return titanHashMap;
+	}
+	public static HashMap<Integer, WeaponRegistry> readWeaponRegistry() throws IOException
+	{
+		HashMap<Integer, WeaponRegistry> weaponHashMap = new HashMap();
+		WeaponRegistry weaponRegistry;
+		
+		try
+		{
+			FileReader fr = new FileReader(WEAPONS_FILE_NAME);
+			BufferedReader br = new BufferedReader(fr);
+			
+			String line = "";
+			while ((line = br.readLine()) != null)
+			{
+				String[] temp = line.split(",");
+				if (temp.length == 4)
+				{
+					weaponRegistry = new WeaponRegistry(Integer.parseInt(temp[0]),
+							Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), temp[3]);
+				}
+				else{
+					weaponRegistry = new WeaponRegistry(Integer.parseInt(temp[0]),
+							Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), temp[3],
+							Integer.parseInt(temp[4]), Integer.parseInt(temp[5]));
+				}
+					
+				weaponHashMap.put(weaponRegistry.getCode(), weaponRegistry);
+			}
+		}
+		catch (IOException e){ e.printStackTrace(); }
+		
+		return weaponHashMap;
 	}
 }
