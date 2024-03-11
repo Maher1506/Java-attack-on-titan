2b595475c5a2e595cabe080fdca5d31983ceaac4
nazmy666 <nazmy2362005@gmail.com>
2024-03-11 17:15:07 +0200

    akbar class bss lsa m5ls4

--- AOT/src/game/engine/weapons/factory/WeaponFactory.java
+++ AOT/src/game/engine/weapons/factory/WeaponFactory.java
@@ -1,5 +1,21 @@
 package game.engine.weapons.factory;
 
-public class WeaponFactory {
+import game.engine.weapons.WeaponRegistry;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+public class WeaponFactory {
+	
+	private final HashMap<Integer , WeaponRegistry> weaponShop;
+	
+	public WeaponFactory() throws IOException
+	{
+		weaponShop = new HashMap();
+	}
+
+	public HashMap<Integer, WeaponRegistry> getWeaponShop() {
+		return weaponShop;
+	}
+	
 }
