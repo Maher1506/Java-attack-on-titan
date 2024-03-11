8ddae559aed8ab3796122cb7e6b487ce6b467b62
nazmy666 <nazmy2362005@gmail.com>
2024-03-11 15:22:50 +0200

    titan regestry 5ls

--- AOT/src/game/engine/titans/AbnormalTitan.java
+++ AOT/src/game/engine/titans/AbnormalTitan.java
@@ -1,6 +1,9 @@
 package game.engine.titans;
 
 public class AbnormalTitan extends Titan {
+	
+	private final int TITAN_CODE = 2;
+	
 	public AbnormalTitan(int baseHealth, int baseDamage, int heightInMeters,
 			int distanceFromBase, int speed, int resourcesValue, int dangerLevel) 
 	{
