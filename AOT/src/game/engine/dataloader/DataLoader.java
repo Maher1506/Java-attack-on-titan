fe326ef1c34a7765d820566080b8f24081cbd3e4
Maher1506 <68017554+Maher1506@users.noreply.github.com>
2024-03-11 22:25:21 +0200

    Added readtitanregistry method in titan in dataloader

--- AOT/src/game/engine/dataloader/DataLoader.java
+++ AOT/src/game/engine/dataloader/DataLoader.java
@@ -3,6 +3,7 @@ package game.engine.dataloader;
 import game.engine.titans.TitanRegistry;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -11,8 +12,32 @@ public class DataLoader {
 	private final String TITANS_FILE_NAME = "titans.csv";
 	private final String WEAPONS_FILE_NAME = "weapons.csv";
 	
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println(readTitanRegistry());
+	}
+	
 	public static HashMap<Integer, TitanRegistry> readTitanRegistry() throws IOException
 	{
-		BufferedReader br = 
+		HashMap<Integer, TitanRegistry> registry = new HashMap();
+		try
+		{
+			FileReader fr = new FileReader("TITANS_FILE_NAME");
+			BufferedReader br = new BufferedReader(fr);
+			
+			String line = "";
+			while ((line = br.readLine()) != null)
+			{
+				String[] temp = line.split(",");
+				TitanRegistry titanRegistry = new TitanRegistry(Integer.parseInt(temp[0]),
+						Integer.parseInt(temp[1]), Integer.parseInt(temp[2]),
+						Integer.parseInt(temp[3]), Integer.parseInt(temp[4]),
+						Integer.parseInt(temp[5]), Integer.parseInt(temp[6]));
+				registry.put(titanRegistry.getCode(), titanRegistry);
+			}
+		}
+		catch (IOException e){ e.printStackTrace(); }
+		
+		return registry;
 	}
 }
