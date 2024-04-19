daf0d1e50f7d2e5798f95629377c31da4279e336
Maher1506 <68017554+Maher1506@users.noreply.github.com>
2024-04-19 20:41:48 +0200

    piercing cannon turnAttack

--- AOT/src/game/engine/lanes/Lane.java
+++ AOT/src/game/engine/lanes/Lane.java
@@ -3,6 +3,7 @@ package game.engine.lanes;
 import java.util.*;
 
 import game.engine.base.Wall;
+import game.engine.titans.AbnormalTitan;
 import game.engine.titans.Titan;
 import game.engine.weapons.Weapon;
 
@@ -13,6 +14,50 @@ public class Lane implements Comparable<Lane> {
 	private final Wall laneWall;
 	private int dangerLevel;
 
+	public static void main(String[] args)
+	{
+		AbnormalTitan t1 = new AbnormalTitan( 0, 0, 0, 100, 0,0,0);
+		AbnormalTitan t2 = new AbnormalTitan( 0, 0, 0, 4000, 0,0,0);
+		AbnormalTitan t3 = new AbnormalTitan( 0, 0, 0, 20, 0,0,0);
+		AbnormalTitan t4 = new AbnormalTitan( 0, 0, 0, 30, 0,0,0);
+		PriorityQueue<Titan> tit = new PriorityQueue<Titan>();
+		tit.offer(t1);
+		tit.offer(t2);
+		tit.offer(t3);
+		tit.offer(t4);
+		
+		PriorityQueue<Titan> tit2 = new PriorityQueue<Titan>();
+		ArrayList<Titan> nazmy2 = new ArrayList<>(tit2);
+		System.out.println(nazmy2.get(0));
+		System.out.println(tit2.peek());
+		System.out.println();
+		
+		ArrayList<Titan> nazmy = new ArrayList<>(tit);
+		for (Titan t: tit)
+		{
+			System.out.println(t.getDistance());
+		}
+		
+		System.out.println();
+		for (Titan t : tit)
+		{
+			tit2.offer(t);
+		}
+		for(Titan t : tit2)
+		{
+			System.out.println(t.getDistance());
+		}
+		
+		System.out.println();
+		System.out.println(tit.peek().getDistance());
+		System.out.println();
+		
+		for(Titan t : tit)
+		{
+			System.out.println(t.getDistance());
+		}
+	}
+	
 	public Lane(Wall laneWall) {
 		this.laneWall = laneWall;
 		titans = new PriorityQueue<Titan>();
@@ -38,7 +83,7 @@ public class Lane implements Comparable<Lane> {
 
 	public  int performLaneTitansAttacks(){
 		int resourcesGathered = 0;
-		for (Titan titan : titans) //CANNOT LOOP THROUGH PriorityQueue
+		for (Titan titan : titans) 
 		{
 			if (titan.hasReachedTarget()){
 				 resourcesGathered += titan.attack(laneWall);
@@ -50,7 +95,7 @@ public class Lane implements Comparable<Lane> {
 	 public int performLaneWeaponsAttacks(){
 		 int resourcesGathered = 0;
 		 for (Weapon weapon : weapons){
-			 //weapon.turnAttack(titans);
+			 weapon.turnAttack(titans);
 		 }
 		 return resourcesGathered;
 	 }
@@ -64,7 +109,7 @@ public class Lane implements Comparable<Lane> {
 	 }
 	 
 	 public void updateLaneDangerLevel(){
-		 for (Titan titan : titans)  //CANNOT LOOP THROUGH PriorityQueue
+		 for (Titan titan : titans)  
 		 {
 			 dangerLevel += titan.getDangerLevel();
 		 }
