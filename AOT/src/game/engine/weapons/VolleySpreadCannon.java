587ab771f22743d09620ef5201b89cec724f12cb
nazmy666 <nazmy2362005@gmail.com>
2024-04-19 23:25:22 +0200

    na2slna elbattle bss
    alooo

--- AOT/src/game/engine/weapons/VolleySpreadCannon.java
+++ AOT/src/game/engine/weapons/VolleySpreadCannon.java
@@ -1,5 +1,9 @@
 package game.engine.weapons;
 
+import game.engine.titans.Titan;
+
+import java.util.PriorityQueue;
+
 public class VolleySpreadCannon extends Weapon {
 	public final static int WEAPON_CODE = 3;
 	private final int minRange;
@@ -18,5 +22,41 @@ public class VolleySpreadCannon extends Weapon {
 	public int getMaxRange() {
 		return maxRange;
 	}
-
+	public int turnAttack(PriorityQueue<Titan> laneTitans){
+		
+		int resourcesGained = 0;
+		if(laneTitans.isEmpty()){
+			resourcesGained = 0;
+		}
+		else {
+		PriorityQueue<Titan> duplicate = laneTitans;
+		PriorityQueue<Titan> temp = new PriorityQueue<Titan>();
+		int pqSize = duplicate.size();
+		for (int i = 0; i < pqSize; i++){
+		if (duplicate.peek().getDistance() > 19 && duplicate.peek().getDistance() > 51 ){
+			
+			int resources = this.attack(duplicate.peek());
+			if(resources != 0)
+			{
+				resourcesGained += resources;
+				duplicate.remove();
+			}
+			else
+			{
+				temp.offer(duplicate.poll());
+			}
+		}
+		else
+		{
+			temp.offer(duplicate.poll());
+		}
+	}
+		laneTitans.clear();
+		for(Titan t : temp)
+		{
+			laneTitans.offer(t);
+		}
+		}
+		return resourcesGained;
+	}	
 }
